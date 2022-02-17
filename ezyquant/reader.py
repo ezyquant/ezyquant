50fb19497b75fa8cc083170d300fd84e336c92ad
Eakkatach <eakkatach@gmail.com>
2022-02-17 19:02:52 +0700

    mock init

--- ezyquant/reader.py
+++ ezyquant/reader.py
@@ -1,3 +1,9 @@
 class SETDataReader:
     def __init__(self, sqlite_path: str) -> None:
+        """
+        The __init__ function is called when an instance of a class is created
+
+        :param sqlite_path: The path to the SQLite database file
+        :type sqlite_path: str
+        """
         self.__sqlite_path = sqlite_path
