0da1bab70ad358340c5f057f905e15ad2ea08046
Eakkatach <eakkatach@gmail.com>
2022-02-21 14:45:24 +0700

    base factor manager

--- ezyquant/factor_manger.py
+++ ezyquant/factor_manger.py
@@ -2,21 +2,27 @@ from datetime import date
 from typing import Iterable
 
 
-class SETUniverseFactorManager:
+class _SETBaseFactorManager:
+    def __init__(self, start_date: date, end_date: date) -> None:
+        self._start_date = start_date
+        self._end_date = end_date
+
+
+class SETUniverseFactorManager(_SETBaseFactorManager):
     def __init__(
         self,
         max_universe: str,
         start_date: date,
         end_date: date,
     ) -> None:
-        pass
+        super().__init__(start_date, end_date)
 
 
-class SETSymbolFactorManager:
+class SETSymbolFactorManager(_SETBaseFactorManager):
     def __init__(
         self,
         symbols: Iterable[str],
         start_date: date,
         end_date: date,
     ) -> None:
-        pass
+        super().__init__(start_date, end_date)
