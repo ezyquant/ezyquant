8b91fcef153b19db067f2b0b042f17ac9aa42c81
Eakkatach <eakkatach@gmail.com>
2022-02-21 14:36:53 +0700

    add ,

---
+++ ezyquant/factor_manger.py
@@ -0,0 +1,22 @@
+from datetime import date
+from typing import Iterable
+
+
+class SETUniverseFactorManager:
+    def __init__(
+        self,
+        max_universe: str,
+        start_date: date,
+        end_date: date,
+    ) -> None:
+        pass
+
+
+class SETSymbolFactorManager:
+    def __init__(
+        self,
+        symbols: Iterable[str],
+        start_date: date,
+        end_date: date,
+    ) -> None:
+        pass
