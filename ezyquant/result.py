972877b3f119b557e8471d3b64e8a6fb8c3cb9bd
Eakkatach <eakkatach@gmail.com>
2022-02-21 17:08:23 +0700

    prepare result

---
+++ ezyquant/result.py
@@ -0,0 +1,7 @@
+class SETResult:
+    def __init__(self) -> None:
+        """
+        do this once finish backtest
+        if backtest time is small -> no daily backtest
+        """
+        pass
