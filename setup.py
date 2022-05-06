d1c7a46d2bad1b0a687091c48e99e0852b1bc750
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-05-06 17:09:28 +0700

    update req

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version=VERSION,
     packages=find_packages(),
     python_requires=">=3.8.0",
-    install_requires=["pandas>=1.3", "sqlalchemy>=1.4"],
+    install_requires=["pandas>=1.4", "sqlalchemy>=1.4"],
 )
