539f830d3b29605324d86725bf8001449ec57bcb
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-06-07 16:37:05 +0700

    update validate

--- ezyquant/backtest/backtest.py
+++ ezyquant/backtest/backtest.py
@@ -57,10 +57,18 @@ def _backtest_target_weight(
                 - price
                 - pct_commission
     """
+    _validate_initial_cash(initial_cash)
+    _validate_pct_commission(pct_commission)
+    _validate_price_df(buy_price_df, sell_price_df)
+    _validate_signal_df(
+        signal_weight_df,
+        trade_date_list=buy_price_df.index.to_list(),
+        symbol_list=buy_price_df.columns.to_list(),
+    )
+
+    # Select only symbol in signal
     buy_price_df = buy_price_df[signal_weight_df.columns]
     sell_price_df = sell_price_df[signal_weight_df.columns]
-    _validate_price_df(buy_price_df, sell_price_df)
-    _validate_signal_df(signal_weight_df, buy_price_df.index)
 
     r_commission = 1.0 + pct_commission
 
@@ -129,6 +137,21 @@ def _backtest_target_weight(
     return cash_s, position_df, trade_df
 
 
+""" 
+Validation
+"""
+
+
+def _validate_initial_cash(initial_cash: float) -> None:
+    if initial_cash < 0:
+        raise ValueError("initial_cash must be positive")
+
+
+def _validate_pct_commission(pct_commission: float) -> None:
+    if pct_commission < 0 or pct_commission > 1:
+        raise ValueError("pct_commission must be between 0 and 1")
+
+
 def _validate_price_df(buy_price_df: pd.DataFrame, sell_price_df: pd.DataFrame):
     idx = buy_price_df.index
 
@@ -140,25 +163,40 @@ def _validate_price_df(buy_price_df: pd.DataFrame, sell_price_df: pd.DataFrame):
         raise ValueError("buy_price_df index must be unique")
     if not idx.equals(sell_price_df.index):
         raise ValueError("buy_price_df and sell_price_df index must be same")
+    if not buy_price_df.columns.equals(sell_price_df.columns):
+        raise ValueError("buy_price_df and sell_price_df columns must be same")
 
     if buy_price_df.empty:
         raise ValueError("buy_price_df must not be empty")
+    if sell_price_df.empty:
+        raise ValueError("sell_price_df must not be empty")
 
 
-def _validate_signal_df(signal_df: pd.DataFrame, trade_date_index: pd.Index):
+def _validate_signal_df(
+    signal_df: pd.DataFrame, trade_date_list: List, symbol_list: List[str]
+):
     idx = signal_df.index
 
+    if not isinstance(idx, pd.DatetimeIndex):
+        raise ValueError("signal_df index must be DatetimeIndex")
     if not idx.is_monotonic_increasing:
         raise ValueError("signal_df index must be monotonic increasing")
     if not idx.is_unique:
         raise ValueError("signal_df index must be unique")
-    if not idx.isin(trade_date_index).all():
-        raise ValueError("signal_df index must be trade date")
+    if not idx.isin(trade_date_list).all():
+        raise ValueError("signal_df index must be in buy_price_df index")
+    if not signal_df.columns.isin(symbol_list).all():
+        raise ValueError("signal_df columns must be in buy_price_df columns")
+
+    if signal_df.empty:
+        raise ValueError("signal_df must not be empty")
 
     signal_df = signal_df.dropna(how="all")
 
     if signal_df.isnull().values.any():
         raise ValueError("signal_df must be nan all row")
+    if (signal_df < 0).values.any():
+        raise ValueError("signal_df must be positive")
     if not signal_df.sum(axis=1).between(0, 1).all():
         raise ValueError(
             "signal_df must be positive and sum must not more than 1 each day"
