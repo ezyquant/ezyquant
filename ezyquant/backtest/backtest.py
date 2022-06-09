0f57cf44b11aee26c0c6c26984e3513c12321c46
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-06-09 11:58:32 +0700

    update backtest rebalance

--- ezyquant/backtest/backtest.py
+++ ezyquant/backtest/backtest.py
@@ -1,6 +1,7 @@
 from dataclasses import fields
 from typing import List, Tuple
 
+import numpy as np
 import pandas as pd
 
 from .. import fields as fld
@@ -13,11 +14,11 @@ from .trade import Trade
 
 
 def backtest_target_weight(
-    sqlite_path: str,
     signal_df: pd.DataFrame,
     rebalance_freq: str,
     rebalance_at: int,
     # common param
+    sqlite_path: str,
     start_date: str,
     end_date: str,
     initial_cash: float,
@@ -29,27 +30,40 @@ def backtest_target_weight(
     trigger_sell_price_mode: str = "close",
     trigger_sell_price_delay_bar: int = 0,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-
     symbol_list = signal_df.columns.tolist()
     ssc = SETSignalCreator(
         sqlite_path=sqlite_path,
         start_date=start_date,
         end_date=end_date,
+        index_list=[],
         symbol_list=symbol_list,
     )
 
-    # TODO: initial_position_dict
+    # Rebalance
+    assert isinstance(signal_df.index, pd.DatetimeIndex)
+    if rebalance_freq == fld.REBALANCE_FREQ_DAILY:
+        is_rebalance_freq = True
+    elif rebalance_freq == fld.REBALANCE_FREQ_WEEKLY:
+        is_rebalance_freq = utils.is_rebalance_weekly(signal_df.index, rebalance_at)
+    elif rebalance_freq == fld.REBALANCE_FREQ_MONTHLY:
+        is_rebalance_freq = utils.is_rebalance_monthly(signal_df.index, rebalance_at)
+    else:
+        raise ValueError(f"Invalid rebalance_freq: {rebalance_freq}")
+
+    is_signal_change = (signal_df != signal_df.shift(1)).any(axis=1)
+
+    is_rebalance = is_signal_change | is_rebalance_freq  # type: ignore
+
+    signal_df = signal_df.where(is_rebalance, np.nan)
 
     # Baned symbol
-    # TODO: Sell only banned symbol, not rebalance
+    # TODO: _backtest_target_weight must allow sell all, but not rebalance
     is_banned = ssc.is_banned()
     signal_df = signal_df.mask(is_banned, 0)
 
-    # Rebalance
-    # TODO: Rebalance by frequency
-    is_signal_change = (signal_df != signal_df.shift(1)).any(axis=1)
-
-    signal_df = signal_df[is_signal_change]
+    # Drop NaN row
+    signal_df = signal_df.dropna(axis=0, how="all")
+    signal_df = signal_df.dropna(axis=1, how="all")
 
     # Price df
     _validate_price_mode(trigger_buy_price_mode)
@@ -81,6 +95,7 @@ def backtest_target_weight(
     # sell_price_df = sell_price_df.shift(trigger_sell_price_delay_bar)
 
     # Backtest
+    # TODO: initial_position_dict
     cash_series, position_df, trade_df = _backtest_target_weight(
         initial_cash=initial_cash,
         signal_weight_df=signal_df,
@@ -90,13 +105,16 @@ def backtest_target_weight(
     )
 
     # Dividend df
+    # TODO: dividend_df
     dividend_df = pd.DataFrame()
 
     # Stat df
+    # TODO: stat_df
     stat_df = pd.DataFrame()
 
     # Summary df
-    summary_df = pd.DataFrame()
+    # TODO: summary_df
+    summary_df = cash_series.to_frame("cash")
 
     return summary_df, position_df, trade_df, dividend_df, stat_df
 
