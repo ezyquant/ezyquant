08f665faa0c0652707178f9eced223ade236d654
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-06-23 16:31:24 +0700

    test: report - add close price

--- tests/test_report/test_stat_df.py
+++ tests/test_report/test_stat_df.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal, assert_series_equal
+from pandas.testing import assert_series_equal
 
-import ezyquant.fields as fld
-from ezyquant.report import SETBacktestReport, position_columns
+from ezyquant.report import SETBacktestReport
 from tests import utils
 
 
