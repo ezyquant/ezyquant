2146433b577cedd273a3b401331c8e3cada49126
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-06-23 15:33:07 +0700

    test: test_maximum_drawdown

--- tests/test_report/test_stat_df.py
+++ tests/test_report/test_stat_df.py
@@ -42,3 +42,40 @@ def test_pct_maximum_drawdown(
             {"port_value": expected_result, "port_value_with_dividend": expected_result}
         ),
     )
+
+
+@pytest.mark.parametrize(
+    ("nav_list", "expected_result"),
+    [
+        ([100.0], 0.0),
+        ([100.0, 200.0], 0.0),
+        ([100.0, 200.0, 300.0], 0.0),
+        ([100.0, 50.0], -50.0),
+        ([100.0, 50.0, 10.0], -90.0),
+        ([100.0, 200.0, 100.0], -100.0),
+        ([100.0, 200.0, 100.0, 400.0, 100.0], -300.0),
+    ],
+)
+def test_maximum_drawdown(
+    sbr: SETBacktestReport, nav_list: pd.DataFrame, expected_result: float
+):
+    # Mock
+    summary_df = pd.DataFrame(
+        {
+            "timestamp": utils.make_bdate_range(len(nav_list)),
+            "port_value": nav_list,
+            "port_value_with_dividend": nav_list,
+        }
+    )
+    sbr.__dict__["summary_df"] = summary_df
+
+    # Test
+    result = sbr.maximum_drawdown
+
+    # Check
+    assert_series_equal(
+        result,
+        pd.Series(
+            {"port_value": expected_result, "port_value_with_dividend": expected_result}
+        ),
+    )
