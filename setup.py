65b8572ee9a837ffd74a38bca45a9b49897da703
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-07-18 15:16:23 +0700

    refactor: add init to test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ VERSION = "0.1.2"
 setup(
     name=NAME,
     version=VERSION,
-    packages=find_packages(),
+    packages=find_packages(include=["ezyquant"]),
     python_requires=">=3.8.0",
     install_requires=["pandas>=1.4", "sqlalchemy>=1.4", "ta>=0.10", "XlsxWriter>=3.0"],
 )
