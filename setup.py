5b57dd77e0a18202cf08aaf3fc3ec6a9a247a611
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-07-19 09:28:23 +0700

    feat: add license

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name=NAME,
     version=VERSION,
     packages=find_packages(include=["ezyquant"]),
-    python_requires=">=3.8.0",
+    python_requires=">=3.8",
     install_requires=["pandas>=1.4", "sqlalchemy>=1.4", "ta>=0.10", "XlsxWriter>=3.0"],
+    license="MIT",
 )
