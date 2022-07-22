49bf57d9b88d55f60e7e6878c8770adaafb1f72f
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-07-22 14:53:14 +0700

    Feat/ez 113 license (#59)
    * refactor: add init to test
    
    * refactor: add init to test
    
    * feat: add license
    
    * feat: setup.py
    
    * chore: docstring
    
    * feat: email
    
    * refactor: license file
    
    * Update setup.py
    
    author CMDF to Fintech
    
    * Update setup.py
    
    change doc website
    
    * doc: docs
    
    Co-authored-by: LightningGL <51393532+LightningGL@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,8 +5,26 @@ VERSION = "0.1.2"
 
 setup(
     name=NAME,
+    packages=find_packages(include=["ezyquant"]),
     version=VERSION,
-    packages=find_packages(),
-    python_requires=">=3.8.0",
+    description="Powerful backtest python library for Thai stocks",
+    long_description="Powerful backtest python library for Thai stocks",
+    author="Fintech (Thailand) Company Limited",
+    author_email="admin@fintech.co.th",
+    url="https://github.com/ezyquant/ezyquant",
+    maintainer="Fintech (Thailand) Company Limited",
+    maintainer_email="admin@fintech.co.th",
     install_requires=["pandas>=1.4", "sqlalchemy>=1.4", "ta>=0.10", "XlsxWriter>=3.0"],
+    license="The MIT License (MIT)",
+    classifiers=[
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    project_urls={
+        "Documentation": "https://doc.ezyquant.com/",
+        "Bug Reports": "https://github.com/ezyquant/ezyquant/issues",
+        "Source": "https://github.com/bukosabino/ta",
+    },
 )
