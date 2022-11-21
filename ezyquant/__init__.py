57d01d5c50b1c737d7fa7e2fe0b74fbb4f22d60b
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2022-11-21 15:03:15 +0700

    Feat/es 4 connect postgres (#97)
    * feat: connect_postgres
    
    * feat: connect_postgres
    
    * feat: setdatareader postgres
    
    * test: connect postgres
    
    * refactor: use func_date only select
    
    * feat: func_date for sqlite, postgres
    
    * Revert "refactor: use func_date only select"
    
    This reverts commit fc3291844a23ee19434ead4ae3671bed3c7f3a82.
    
    * refactor: remove func date from orderby
    
    * fix: groupby
    
    * doc: todo
    
    * refactor: func_date
    
    * refactor: connent.py
    
    * doc: todo

--- ezyquant/__init__.py
+++ ezyquant/__init__.py
@@ -1,6 +1,6 @@
 from ._version import __version__
 from .backtesting import backtest
-from .connect import connect_sqlite
+from .connect import connect_postgres, connect_sqlite
 from .creator import SETSignalCreator
 from .reader import SETDataReader
 from .report import SETBacktestReport
--- ezyquant/connect.py
+++ ezyquant/connect.py
@@ -1,3 +1,8 @@
+import os.path
+
+import sqlalchemy as sa
+
+from .errors import InputError
 from .reader import SETDataReader, _SETDataReaderCached
 
 
@@ -9,6 +14,37 @@ def connect_sqlite(sqlite_path: str):
     sqlite_path: str
         SQLite database location path (e.g. C:/.../ezyquant.db)
     """
+    if not os.path.isfile(sqlite_path):
+        raise InputError(f"{sqlite_path} is not found")
+
+    url = f"sqlite:///{sqlite_path}"
+    return _set_engine(url)
+
+
+def connect_postgres(host: str, username: str, password: str, port: str, database: str):
+    """Connect to PostgreSQL database.
+
+    Parameters
+    ----------
+    host: str
+        PostgreSQL host (e.g. localhost)
+    username: str
+        PostgreSQL username (e.g. postgres)
+    password: str
+        PostgreSQL password (e.g. password)
+    port: str
+        PostgreSQL port (e.g. 5432)
+    database: str
+        PostgreSQL database name (e.g. ezyquant)
+    """
+    url = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+    return _set_engine(url)
+
+
+def _set_engine(url: str):
+    engine = sa.create_engine(url)
+
     _SETDataReaderCached.cache_clear()
-    SETDataReader._sqlite_path = sqlite_path
+    SETDataReader._engine = engine
+
     return SETDataReader()
