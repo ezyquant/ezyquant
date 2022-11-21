fd414f6ba4b727a05e1ae77f25eb18fc32eb89e3
Kongtap (Arm) Arunlakvilart <kongtap@fintech.co.th>
2022-11-21 14:29:42 +0700

    refactor: connent.py

--- ezyquant/connect.py
+++ ezyquant/connect.py
@@ -17,12 +17,8 @@ def connect_sqlite(sqlite_path: str):
     if not os.path.isfile(sqlite_path):
         raise InputError(f"{sqlite_path} is not found")
 
-    engine = sa.create_engine(f"sqlite:///{sqlite_path}")
-
-    _SETDataReaderCached.cache_clear()
-    SETDataReader._engine = engine
-
-    return SETDataReader()
+    url = f"sqlite:///{sqlite_path}"
+    return _set_engine(url)
 
 
 def connect_postgres(host: str, username: str, password: str, port: str, database: str):
@@ -42,8 +38,13 @@ def connect_postgres(host: str, username: str, password: str, port: str, databas
         PostgreSQL database name (e.g. ezyquant)
     """
     url = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+    return _set_engine(url)
+
+
+def _set_engine(url: str):
     engine = sa.create_engine(url)
 
     _SETDataReaderCached.cache_clear()
     SETDataReader._engine = engine
+
     return SETDataReader()
