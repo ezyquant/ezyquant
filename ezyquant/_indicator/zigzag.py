421fe6d5801103cec42e38568a7aabb241d8f66d
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2023-06-08 09:41:03 +0700

    refactor: dir indicator

---
+++ ezyquant/_indicator/zigzag.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+PEAK, VALLEY = 1, -1
+
+
+def _identify_initial_pivot(X, up_thresh, down_thresh):
+    """Quickly identify the X[0] as a peak or valley."""
+    x_0 = X[0]
+    max_x = x_0
+    max_t = 0
+    min_x = x_0
+    min_t = 0
+    up_thresh += 1
+    down_thresh += 1
+
+    for t in range(1, len(X)):
+        x_t = X[t]
+
+        if x_t / min_x >= up_thresh:
+            return VALLEY if min_t == 0 else PEAK
+
+        if x_t / max_x <= down_thresh:
+            return PEAK if max_t == 0 else VALLEY
+
+        if x_t > max_x:
+            max_x = x_t
+            max_t = t
+
+        if x_t < min_x:
+            min_x = x_t
+            min_t = t
+
+    t_n = len(X) - 1
+    return VALLEY if x_0 < X[t_n] else PEAK
+
+
+def peak_valley_pivots_candlestick(close, high, low, up_thresh, down_thresh):
+    """
+    Finds the peaks and valleys of a series of HLC (open is not necessary).
+    TR: This is modified peak_valley_pivots function in order to find peaks and valleys for OHLC.
+    Parameters
+    ----------
+    close : This is series with closes prices.
+    high : This is series with highs  prices.
+    low : This is series with lows prices.
+    up_thresh : The minimum relative change necessary to define a peak.
+    down_thesh : The minimum relative change necessary to define a valley.
+    Returns
+    -------
+    an array with 0 indicating no pivot and -1 and 1 indicating valley and peak
+    respectively
+    Using Pandas
+    ------------
+    For the most part, close, high and low may be a pandas series. However, the index must
+    either be [0,n) or a DateTimeIndex. Why? This function does X[t] to access
+    each element where t is in [0,n).
+    The First and Last Elements
+    ---------------------------
+    The first and last elements are guaranteed to be annotated as peak or
+    valley even if the segments formed do not have the necessary relative
+    changes. This is a tradeoff between technical correctness and the
+    propensity to make mistakes in data analysis. The possible mistake is
+    ignoring data outside the fully realized segments, which may bias analysis.
+    """
+    if down_thresh > 0:
+        raise ValueError("The down_thresh must be negative.")
+
+    initial_pivot = _identify_initial_pivot(close, up_thresh, down_thresh)
+
+    t_n = len(close)
+    pivots = np.zeros(t_n, dtype="i1")
+    pivots[0] = initial_pivot
+
+    # Adding one to the relative change thresholds saves operations. Instead
+    # of computing relative change at each point as x_j / x_i - 1, it is
+    # computed as x_j / x_1. Then, this value is compared to the threshold + 1.
+    # This saves (t_n - 1) subtractions.
+    up_thresh += 1
+    down_thresh += 1
+
+    trend = -initial_pivot
+    last_pivot_t = 0
+    last_pivot_x = close[0]
+    for t in range(1, len(close)):
+        if trend == -1:
+            x = low[t]
+            r = x / last_pivot_x
+            if r >= up_thresh:
+                pivots[last_pivot_t] = trend  #
+                trend = 1
+                # last_pivot_x = x
+                last_pivot_x = high[t]
+                last_pivot_t = t
+            elif x < last_pivot_x:
+                last_pivot_x = x
+                last_pivot_t = t
+        else:
+            x = high[t]
+            r = x / last_pivot_x
+            if r <= down_thresh:
+                pivots[last_pivot_t] = trend
+                trend = -1
+                # last_pivot_x = x
+                last_pivot_x = low[t]
+                last_pivot_t = t
+            elif x > last_pivot_x:
+                last_pivot_x = x
+                last_pivot_t = t
+
+    if last_pivot_t == t_n - 1:
+        pivots[last_pivot_t] = trend
+    elif pivots[t_n - 1] == 0:
+        pivots[t_n - 1] = trend
+
+    return pivots
--- ezyquant/indicators.py
+++ ezyquant/indicators.py
@@ -1,10 +1,8 @@
-import operator
 from typing import Callable, Tuple
 
 import numpy as np
 import pandas as pd
-import zigzag
-from ta.momentum import ROCIndicator, RSIIndicator, StochasticOscillator, rsi
+from ta.momentum import ROCIndicator, RSIIndicator, StochasticOscillator
 from ta.trend import MACD, ADXIndicator, CCIIndicator, IchimokuIndicator, PSARIndicator
 from ta.utils import _ema, _sma
 from ta.volatility import (
@@ -14,6 +12,7 @@ from ta.volatility import (
     KeltnerChannel,
 )
 
+from ._indicator.rsi_divergence import rsi_divergence
 from .errors import InputError
 
 nan = float("nan")
@@ -712,110 +711,3 @@ def _apply_t(series: pd.Series, func: Callable) -> pd.DataFrame:
         raise InputError(f"{func.__name__} returned an empty dataframe")
     assert isinstance(df, pd.DataFrame)
     return df
-
-
-"""
-Pivot
-"""
-
-
-def pivot_points_high(high: pd.Series, left_bars: int, right_bars: int) -> pd.Series:
-    """Pivot Points High.
-
-    High is the highest high in a window between left_bars and
-    right_bars. If multiple bars have the same high, the most right bar
-    is used.
-    """
-    max_ = high.rolling(window=left_bars + 1).max()
-    max_right = high.rolling(window=right_bars).max().shift(-right_bars)
-
-    return high.where((max_ == high) & (max_ > max_right))
-
-
-def pivot_points_low(low: pd.Series, left_bars: int, right_bars: int) -> pd.Series:
-    """Pivot Points Low.
-
-    Low is the lowest low in a window between left_bars and right_bars.
-    If multiple bars have the same low, the most right bar is used.
-    """
-    min_ = low.rolling(window=left_bars + 1).min()
-    min_right = low.rolling(window=right_bars).min().shift(-right_bars)
-
-    return low.where((min_ == low) & (min_ < min_right))
-
-
-"""
-RSI Divergence
-"""
-
-
-def rsi_divergence(
-    close: pd.Series,
-    rsi_period: int = 14,
-    pivot_up_thresh: float = 0.05,
-    pivot_down_thresh: float = -0.05,
-) -> pd.Series:
-    """Return Positive RSI of the bullish divergence points and Negative RSI of
-    the bearish divergence points.
-
-    Parameters
-    ----------
-    high : pd.Series
-        Series of 'high' prices.
-    low : pd.Series
-        Series of 'low' prices.
-    close : pd.Series
-        Series of 'close' prices.
-    rsi_period : int, optional
-        RSI period, by default 14
-    pivot_up_thresh : int
-        Pivot up threshold, by default 0.05
-    pivot_down_thresh : int
-        Pivot down threshold, by default -0.05
-    """
-    # Calculate RSI
-    rsi_ = rsi(close, window=rsi_period)
-
-    # Calculate pivot points using zigzag
-    zz_ = zigzag.peak_valley_pivots(  # type: ignore
-        close.to_numpy(),
-        up_thresh=pivot_up_thresh,
-        down_thresh=pivot_down_thresh,
-    )
-    close_pl = close[zz_ < 0]
-    close_ph = close[zz_ > 0]
-
-    # Calculate Divergence
-    bullish = _divergence(close_pl, rsi_, bullish=True)
-    bearish = _divergence(close_ph, rsi_, bullish=False)
-
-    return bullish.fillna(-bearish)
-
-
-def _divergence(
-    price_pivot: pd.Series, indicator: pd.Series, bullish: bool = True
-) -> pd.Series:
-    """Divergence.
-
-    Parameters
-    ----------
-    price_pivot : pd.Series
-        Price pivot points. low for bullish divergence and high for bearish divergence.
-    indicator : pd.Series
-        indicator series.
-    bullish : bool, optional
-        bullish or bearish divergence, by default True
-
-    Returns
-    -------
-    pd.Series
-        Divergence series. Not NaN when divergence occurs.
-    """
-    indicator_pivot = indicator[price_pivot.index]
-
-    op = operator.lt if bullish else operator.gt
-
-    return indicator.where(
-        op(price_pivot, price_pivot.shift(1))
-        & op(indicator_pivot.shift(1), indicator_pivot)
-    )
