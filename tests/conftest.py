0abdc0a6c24eb53e84e39044367bd8a5ebdbbb35
Kongtap Arunlakvilart <kongtap@fintech.co.th>
2023-06-20 09:58:31 +0700

    test: env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,6 @@
 import pytest
 
-import ezyquant as ez
 from ezyquant import SETDataReader
-
-
-@pytest.fixture(scope="session")
-def sqlite_path() -> str:
-    return "ezyquant.db"
-
-
-@pytest.fixture(scope="session")
-def connect_sqlite(sqlite_path: str):
-    ez.connect_sqlite(sqlite_path)
-
-
-@pytest.fixture(scope="session")
-def connect_postgres():
-    ez.connect_postgres(
-        host="localhost",
-        username="postgres",
-        password="postgres",
-        port="5432",
-        database="psims",
-    )
 
 
 @pytest.fixture
